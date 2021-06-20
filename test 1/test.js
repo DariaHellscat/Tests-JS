3757a6431ca88eb479ac2eb61e1c3af15cba2bb8
Daria Golubnichaia <daryadeveloper14@gmail.com>
2021-06-20 14:08:48 +0300

    test.js command switch was learned

--- test 1/test.js
+++ test 1/test.js
@@ -9,3 +9,21 @@ if (num < 49) {
 }
 
 num == 50 ? console.log("Верно!") : console.log("Неверно!");
+
+switch (num) {
+  case num < 49:
+    console.log("Неверно!");
+    break;
+  case num > 100:
+    console.log("Много!");
+    break;
+  case num > 80:
+    console.log("Всё ещё много!");
+    break;
+  case 50:
+    console.log("Верно!");
+    break;
+  default:
+    console.log("Что-то пошло не так");
+    break;
+}
